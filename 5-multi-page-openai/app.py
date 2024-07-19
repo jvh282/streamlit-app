d15a21171b9cd9495791b8eb94eb11ce273d6adc
jvh282 <jh282@hdm-stuttgart.de>
2024-07-19 02:57:47 +0200

    update

--- 5-multi-page-openai/app.py
+++ 5-multi-page-openai/app.py
@@ -4,11 +4,11 @@ import user_persona
 import company_info
 import openai_model
 import data
+import explain
 
-# To use this app, you need an .env file with the OPENAI API
-# and you need to fill in the ID in the assistant.py file
 
 PAGES = {
+    "Erklärung": explain,
     "User Persona": user_persona,
     "Company Information": company_info,
     "Data": data,
