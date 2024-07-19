34fcac41084b6b4863a5f513d6813890487a61c9
jvh282 <jh282@hdm-stuttgart.de>
2024-07-19 03:13:59 +0200

    update

--- 5-multi-page-openai/user_persona.py
+++ 5-multi-page-openai/user_persona.py
@@ -17,7 +17,7 @@ def app():
         # Verhaltensdaten
         nutzung_sozialer_medien = st.text_area("Nutzungsverhalten Sozialer Medien")
         markenpraferenzen = st.text_area("Markenpräferenzen")
-        markenloyalitaet= st.slider("Ausprägung","niedrig", "hoch", "mittel")
+        markenloyalitaet= st.text_input ("Markenloyalität")
 
         # Knopf zum Absenden des Formulars
         submit_button = st.form_submit_button("User Persona erstellen")
